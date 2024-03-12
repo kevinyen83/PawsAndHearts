007ce60cf6d66d232ccf59a06b1ffd6bcc5380e6
Kevin <kevinyenhaha@gmail.com>
2024-03-12 15:30:40 +1100

    v4.2 Updated RWD for the navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -6,12 +6,15 @@ import Image from 'next/image'
 import Link from 'next/link'
 import Button from './Button'
 import { CATEGORY_LINKS } from '../constants'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
 import AvatarIcon from '../assets/images/avatar.png';
 
 
 export default function Navbar () {
     const { data: session } = useSession();
     const [userEmail, setUserEmail] = useState<string | null>(null);
+    const [showDropdown, setShowDropdown] = useState<boolean>(false);
 
 
     useEffect(() => {
@@ -22,17 +25,20 @@ export default function Navbar () {
         }
     }, [session]);
 
+    const toggleDropdown = () => {
+        setShowDropdown(!showDropdown);
+    }
 
   return (
     <nav className='fixed top-0 left-0 right-0 bg-white border-gray-500 flexBetween padding-container z-30 py-4'>
         <ul className='flex flex-row h-full justify-start gap-12 lg:flex items-center'>
-            <div className=''>
+            <div>
                 <Link href='/'>
                     <Image src='/logo.png' alt='logo' width={80} height={29} />
                 </Link>
             </div>
             {CATEGORY_LINKS.map((link) => (
-            <div className='relative group ml-3' key={link.key}>
+            <div className='relative group ml-3 hidden lg:flex' key={link.key}>
                 <Link href={link.href} className='regular-16 text-gray-50 flexCenter cursor-pointer pb-1.5 transition-all hover:font-bold'>
                     {link.label}
                 </Link>
@@ -40,8 +46,9 @@ export default function Navbar () {
         ))}
       </ul>
 
+    {/* Desktop navigation */}
+    <div className='flex justify-between'>
         {session ? (
-            <div className='flex justify-between'>
                 <div className='lg:flexCenter hidden'>
                     <div className='flex items-center space-x-2'>
                         <Button 
@@ -57,7 +64,6 @@ export default function Navbar () {
                         </Link>
                     </div>
                 </div>
-            </div>
             ) : (
                 <div className='lg:flexCenter hidden'>
                     <Link href='/api/auth/signin'>
@@ -67,6 +73,43 @@ export default function Navbar () {
                         variant='btn_white'
                         />
                     </Link>
+                </div>
+            )}
+        </div>
+
+         {/* Dropdown menu icon */}
+         <div className='lg:hidden flex items-center'>
+                <button onClick={toggleDropdown}>
+                    <FontAwesomeIcon icon={faBars} className='h-6 w-6 text-gray-500 cursor-pointer' />
+                </button>
+            </div>
+
+            {/* Dropdown list */}
+            {showDropdown && (
+                <div className='lg:hidden absolute right-6 top-16 bg-white border border-gray-200 rounded-md shadow-md my-2'>
+                    <ul className='flex flex-col gap-0.5'>
+                        {CATEGORY_LINKS.map((link) => (
+                            <div key={link.key}>
+                                <Link href={link.href} passHref>
+                                    <div className='block py-2 px-4 hover:bg-gray-100 transition duration-200'>{link.label}</div>
+                                </Link>
+                            </div>
+                        ))}
+                        <hr className='border-gray-200' />
+                        {session ? (
+                            <li>
+                                <Link href='/api/auth/signout' passHref>
+                                    <div className='block py-2 px-4 hover:bg-gray-100 transition duration-200'>Sign Out</div>
+                                </Link>
+                            </li>
+                        ) : (
+                            <li>
+                                <Link href='/api/auth/signin' passHref>
+                                    <div className='block py-2 px-4 hover:bg-gray-100 transition duration-200'>Login</div>
+                                </Link>
+                            </li>
+                        )}
+                    </ul>
                 </div>
             )}
     </nav>
