d7ccd023719253cf721125461fde54dff027914b
Kevin <kevinyenhaha@gmail.com>
2024-05-26 17:32:51 +1000

    Moved petProfile type outside of the file

---
+++ types/petProfile-types.ts
@@ -0,0 +1,18 @@
+export interface PetProfileData {
+  petId: string;
+  contactEmail: string;
+  contactPhone: string;
+  organizationName: string;
+  applicantName: string;
+  name: string;
+  category: string;
+  age: string;
+  gender: string;
+  color: string;
+  size: string;
+  location: string;
+  vaccination: string;
+  availability: string;
+  image: string;
+  description: string;
+}
