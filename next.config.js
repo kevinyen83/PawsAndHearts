186186b41dd5823eef80944611a03acb59f3506b
Kevin <kevinyenhaha@gmail.com>
2024-05-29 22:29:08 +1000

    Updated env.local

--- next.config.js
+++ next.config.js
@@ -1,5 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: { domains: ["images.ctfassets.net"] }, 
-  }
-module.exports = nextConfig
+    images: { 
+      domains: ["images.ctfassets.net"]
+    },
+    env: {
+      AWS_API_KEY: process.env.AWS_API_KEY,
+    },
+  };
+  
+  module.exports = nextConfig;
+  
